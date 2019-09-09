4872dc92b9b34ca9769ac13b142b83aac38d0e7d
Carlos J. Soto <csoto09@gmail.com>
2019-09-08 21:37:00 -0400

    in progress testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+describe('Examining syntax of Jest tests', () => {
+
+  it('renders without crashing', () => {
+    mount(<App />)
+  })
+
 });

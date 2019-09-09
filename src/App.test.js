6cc1a76168e16b8ac9327dbc345275c1b1003aa7
Carlos J. Soto <csoto09@gmail.com>
2019-09-09 18:00:19 -0400

    tests for App component complete

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,32 @@ import React from 'react';
 import { shallow, mount } from 'enzyme'
 import App from './App'
 
+const newCity = {
+  label: 'test',
+  name: 'test',
+  lat: '1',
+  lng: '2'
+}
 
 
-describe('Examining syntax of Jest tests', () => {
+describe('Examine basic functionality of component', () => {
 
-  it('renders without crashing', () => {
+  it('render App component without crashing', () => {
+    shallow(<App />)
+  })
+
+  it('renders all components without crashing', () => {
     mount(<App />)
   })
 
+  it('should return 0 when no cities have been entered', () => {
+    const app = shallow(<App />)
+    expect(app.state().cities.length).toBe(0)
+  })
+
+  it('should update cities array when adding a new city', () => {
+    const app = shallow(<App />)
+    app.setState({ cities: [...app.state().cities, newCity] })
+    expect(app.state().cities.length).toBe(1)
+  })
 });

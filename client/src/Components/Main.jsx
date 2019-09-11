824274f278e0632437d79cb822881f020a623b89
Carlos J. Soto <csoto09@gmail.com>
2019-09-11 16:48:20 -0400

    adding icons to weather display

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-// import Weather from './Entry/Weather'
-// import WeatherIcon from 'react-icons-weather';
+import WeatherIcon from 'react-icons-weather';
 
 const Main = (props) => {
-  return (
-    <section>
-      <Jumbotron className='mx-auto'>
-        <h2>{props.activeEntry}</h2>
-        <h3>{props.currentWeather ? Math.round(props.currentWeather.temperature) + '\u00B0' : ''}</h3>
-        <h4>{props.currentWeather.summary}</h4>
-      </Jumbotron>
-    </section>
-  )
+  if (props.currentWeather) {
+    return (
+      <section>
+        <Jumbotron className='mx-auto'>
+          <h2>{props.activeEntry}</h2>
+          <h3>
+            <WeatherIcon name='darksky' iconId={props.currentWeather.icon} /><span> </span>
+            {Math.round(props.currentWeather.temperature) + '\u00B0'}F
+          </h3>
+          <h4>{props.currentWeather.summary}</h4>
+        </Jumbotron>
+      </section >
+    )
+  } else {
+    return (
+      <section>
+        <Jumbotron className='mx-auto'>
+          <h2>Weather by Bloc</h2>
+        </Jumbotron>
+      </section>
+    )
+  }
+
 }
 
 export default Main
